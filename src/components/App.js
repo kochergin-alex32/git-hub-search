6e6654f8fe12a8adaab257441790bc275c19ee1f
kochergin-alex32 <kochergin.a.v@bk.ru>
2023-12-10 03:18:42 +0300

    немного продвинулся с запросами

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,116 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
+import React, { useState, useEffect,  } from "react";
+import "./App.css";
 
 import Header from "./Header/Header";
 
 import Cards from "./Cards/Cards";
 import CardPerson from "./CardPerson/CardPerson";
 
-
-import{BrowserRouter, Route, Routes}from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount]= useState([]);
-  
+  const [count, setCount] = useState([]);
+  const [repos, setRepos] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
   const logins = [];
-  const [searchValue, setSearchValue] = useState('');
+  const urls = [];
+  const arr = [];
   // console.log(searchValue,'pomenalos');
-  useEffect(()=>{
-    fetch(`https://api.github.com/users`,{
-      method:"GET",
-      headers:{
-        Authorization:"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"
-      }
-      
-    }).then((res)=>res.json()).then(data=>{setCount(data)})
-  },[setCount]);
-  
-  
-    // console.log(count);
-   count!==0 && count.forEach((users)=>{
-       logins.push(users.login)
+  useEffect(() => {
+    fetch(`https://api.github.com/users`, {
+      method: "GET",
+      headers: {
+        Authorization: "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",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCount(data);
       });
+  }, [setCount]);
+
+  // console.log(count);
+  count !== 0 &&
+    count.forEach((users) => {
+      logins.push(users.login);
+    });
+    count!==0 && count.forEach((users)=>{
+        urls.push(users.url)
+       });
+
+// console.log(urls);
+
+//  const reposHand= useMemo(()=>{
+//          urls.map((ind)=>{
+//              fetch(ind,{
+//      method:"GET",
+//           headers:{
+//             Authorization:""
+//           }
+          
+//         }).then((res)=>res.json()).then(data=>{
+//       arr.push(data)
+//          // console.log(arr);
+//          setRepos(arr)
+//        })
+//        })
+// },[])
+// console.log(reposHand);
+ useEffect(()=>{
+       
+   
+       
+       
+       urls.map((ind)=>{
+             fetch(ind,{
+     method:"GET",
+          headers:{
+            Authorization:""
+          }
+          
+        }).then((res)=>res.json()).then(data=>{
+      arr.push(data)
+         // console.log(arr);
+         setRepos(arr)
+       })
+       })
     
-      
-      
-      
-      // fetch(`https://api.github.com/users/`).then((res)=>res.json()).then(d=>{setRepos(d)})
-      
- 
+    
+       },[count, setCount, setRepos]);
+console.log(repos);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header searchValue={searchValue} setSearchValue={setSearchValue} />
+
+        <div className="content col-12 bg-dark">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Cards
+                  count={count}
+                  logins={logins}
+                  searchValue={searchValue}
+                  repos={repos}
+                  setRepos={setRepos}
+                />
+              }
+            />
+
+            <Route path="count/:id" element={<CardPerson count={count} />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+// fetch(`https://api.github.com/users/`).then((res)=>res.json()).then(d=>{setRepos(d)})
 
 //     function ff() {
 // return new Promise(logins => setTimeout(logins, 500));
@@ -50,27 +124,3 @@ function App() {
 
 // }
 // })();
-   
-
-
-  return (
-   <BrowserRouter>
-      <div className="App">
-      
-      <Header searchValue={searchValue} setSearchValue={setSearchValue}/>
-      
-        <div className='content col-12 bg-dark'>
-
-            <Routes>
-                <Route path='/' element={<Cards count={count} logins={logins} searchValue={searchValue} />}/>
-                
-                <Route path='count/:id' element={<CardPerson count={count} />}/>
-                
-            </Routes>
-        </div>
-      </div>
-  </BrowserRouter>
-  );
-}
-
-export default App;
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -5,75 +5,52 @@ import '../Cards/Cards.css'
 import NotFound from '../NotFound/NotFound';
 
 
-function Cards({count, logins, searchValue}) {
-  const [repos, setRepos] = useState([]);
-  // console.log(searchValue);
-   const users = count.filter((obj)=>{
+function Cards({count, repos, setRepos,searchValue}) {
+ 
+  
+  
+ 
+   
+
+ 
+  const users = repos.filter((obj)=>{
     if(obj.login.toLowerCase().includes(searchValue.toLowerCase())){
       return true
     }
     return false
    })
 
-   console.log(logins);  
-  //  useEffect(()=>{
-  //    fetch(`https://api.github.com/users/defunkt`,{
-  //      method:"GET",
-  //      headers:{
-  //        Authorization:""
-  //      }
-       
-  //    }).then((res)=>res.json()).then(data=>{setRepos(data)})
-  //  },[logins]);
-
  
+  
 
-  //  console.log(repos);
-
- 
-
-//        function ff() {
-// return new Promise(logins => setTimeout(logins, 1000));
-// }
-
-// (async function() {
-// for (let i = 0; i < logins.length; i++) {
-
-// await ff();
-// // console.log('nahuy', logins[i]);
-// fetch(`https://api.github.com/users/{logins[i]}`,{
-//        method:"GET",
-//        headers:{
-//          Authorization:""
-//        }
-       
-//      }).then((res)=>res.json()).then(data=>{setRepos(data)})
-// console.log(repos);
-// }
-// })();
-
-  //  console.log(users);
- 
+  
   return (
-    <>
-{users.length==0 && count.length!==0 ? <NotFound searchValue={searchValue}/> :
 
-(users.map((id)=>((count.length!==0 )?   <Link to={`count/${id.login}`}  key='ind' className='text-decoration-none card col-sx-6 col-md-4 bg-dark'>
-      <div className='img col-3 bg-light'>
-          <img  className='img'src={id.avatar_url} alt="" />
-          
+
+    <>
+    {users.length==0 && count.length!==0 ? <NotFound searchValue={searchValue}/> :
+    
+    (users.map((id)=>((count.length!==0 && repos.length===30 )?   <Link to={`count/${id.login}`}  key='ind' className='text-decoration-none card col-sx-6 col-md-4 bg-dark'>
+          <div className='img col-3 bg-light'>
+              <img  className='img'src={id.avatar_url} alt="" />
+              
+              </div>
+          <div className='cardBody col-8 ' >
+              <span > <a className='text-danger' href="">{id.login}</a>, {id.public_repos} репозиториев</span>
+              <p >{id.company}</p>
           </div>
-      <div className='cardBody col-8 ' >
-          <span > <a className='text-danger' href="">{id.login}</a>, 15 репозиториев</span>
-          <p >hazvanie jrganizacii</p>
-      </div>
-  </Link> : ''))
-) 
-}
-    
-    
-    
-    </>
+      </Link> : ''))
+    ) 
+    }
+        
+        
+        
+        </>
+
+
+
+
+
   )
 }
 
@@ -82,8 +59,31 @@ export default Cards
 
 
 
-{/* <h1 className='text-danger'>not faund</h1>  */}
+
      
 
 
- 
+{/* <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div> */}
+
+
+//     <>
+// {users.length==0 && count.length!==0 ? <NotFound searchValue={searchValue}/> :
+
+// (users.map((id)=>((count.length!==0 )?   <Link to={`count/${id.login}`}  key='id' className='text-decoration-none card col-sx-6 col-md-4 bg-dark'>
+//       <div className='img col-3 bg-light'>
+//           <img  className='img'src={id.avatar_url} alt="" />
+          
+//           </div>
+//       <div className='cardBody col-8 ' >
+//           <span > <a className='text-danger' href="">{id.login}</a>, 15 репозиториев</span>
+//           <p >hazvanie jrganizacii</p>
+//       </div>
+//   </Link> : ''))
+// ) 
+// }
+    
+    
+    
+//     </>
+
+
